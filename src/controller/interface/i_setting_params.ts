3d0f9cc5b8dbebfd05f7aaf98f60fb120a596f00
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2021-05-04 20:09:54 +0200

    Interface SQL

---
+++ src/controller/interface/i_setting_params.ts
@@ -0,0 +1,12 @@
+export interface ISettingParams
+{
+    values?:any, 
+    set?:any, 
+    where?:any, 
+    in?:any, 
+    like?:any, 
+    findset?:any, 
+    order?:any, 
+    limit?:any, 
+    insert?:any
+}
