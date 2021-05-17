619c6b98b06c7d2090bcacae569af6dddbaad60c
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2021-05-17 20:53:36 +0200

    Update about.ts

--- src/router/www/get/about.ts
+++ src/router/www/get/about.ts
@@ -4,25 +4,28 @@ export default class index
 {
     static async html()
     {
-        return '<h1>Pagina about</h1>'
+        //console.log(database)
 
+        
         let db = new database()
-
-        let sql = 
+        
+        /*let sql = 
         {
             values:['id','name','name:nickname'],
             where:{ id:1, name:['hola','adios']}
         }
-
+        
         // SELECT id, name, name AS nickname FROM user WHERE id = 1 AND ( name='hola' OR name= 'adios ')
-
+        
         let data = await db.findAll('user', sql)
-
+        
         if(db.isEmpty())
         {
-
+            
         } else {
-
-        }
+            
+        }*/
+        
+        return '<h1>Pagina about</h1>'
     }
 }
