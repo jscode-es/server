708093e0f1000ce99fc6cbb2373b56bab7f79ef5
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2021-05-17 20:53:52 +0200

    Create tutorial.ts

---
+++ src/router/www/get/tutorial.ts
@@ -0,0 +1,31 @@
+import database from './../../../controller/database'
+
+export default class tutorial
+{
+    static async html()
+    {
+        //console.log(database)
+
+        
+        let db = new database()
+        
+        /*let sql = 
+        {
+            values:['id','name','name:nickname'],
+            where:{ id:1, name:['hola','adios']}
+        }
+        
+        // SELECT id, name, name AS nickname FROM user WHERE id = 1 AND ( name='hola' OR name= 'adios ')
+        
+        let data = await db.findAll('user', sql)
+        
+        if(db.isEmpty())
+        {
+            
+        } else {
+            
+        }*/
+        
+        return '<h1>Pagina tutorial</h1>'
+    }
+}
