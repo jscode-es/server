efd76b2f903370531d725ace16a52c59f3985f90
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2021-05-17 20:53:43 +0200

    Update index.ts

--- src/router/www/get/index.ts
+++ src/router/www/get/index.ts
@@ -1,11 +1,29 @@
 import render from './../../../controller/render'
+import database from './../../../controller/database'
 
 export default class index
 {
     static async html()
     {
-        let html = render.file('www/index')
+        let publish = await index.getPublish()
+
+        let html = render.file('www/index',{ publish })
         
         return html
+    }
+
+    private static async getPublish()
+    {
+        let db = new database()
+
+        let sql = `SELECT b.id, b.title, b.subtitle, b.portrait, b.created, b.url, bt.name AS blog_type FROM blog b
+        INNER JOIN blog_type bt ON b.blog_type = bt.id
+        WHERE b.isVisible = 1
+        ORDER BY b.created DESC 
+        LIMIT 4`
+
+        let res = await db.query(sql)
+
+        return res
     }
 }
