c8b3b3d14c8afcebecae4c6f7e5a9d2e7140f673
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-04-18 14:27:43 +0200

    Update index.tsx

--- src/components/btn/index.tsx
+++ src/components/btn/index.tsx
@@ -1,12 +1,14 @@
 import style from './style.module.css'
 
-import Text from '../text';
+import Text from '../text'
+import { FaTwitch } from "react-icons/fa"
 
 export default function Btn() {
 
 
     return (
         <a className={style.container} href="https://www.twitch.tv/jscode_/videos?filter=archives&sort=time">
+            <FaTwitch />
             <Text>Ver diferido</Text>
         </a>
     )
