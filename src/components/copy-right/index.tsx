222058893285ae88f799dccc6a7990bf1ffd9b47
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-04-18 14:27:52 +0200

    Update index.tsx

--- src/components/copy-right/index.tsx
+++ src/components/copy-right/index.tsx
@@ -5,7 +5,7 @@ import Text from '../text'
 export default function CopyRight({ children }: any) {
     return (
         <div className={style.container}>
-            <Text>JSCode © 2011 - 2022</Text>
-            <Text>All rights reserved</Text>
+            <Text className="copyRight">JSCode © 2011 - 2022</Text>
+            <Text className="copyRight">All rights reserved</Text>
         </div>)
 }
