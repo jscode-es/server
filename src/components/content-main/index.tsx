a4ab42dd167322940246dcb34cbf9e5aea9971f0
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-04-18 14:27:49 +0200

    Update index.tsx

--- src/components/content-main/index.tsx
+++ src/components/content-main/index.tsx
@@ -7,7 +7,7 @@ export default function ContentMain({ children }: any) {
     return (
         <div className={style.container}>
             <Text type="h4">No se trata de donde vienes, sino a donde vas</Text>
-            <Text type="h1">Desarrollor de software<br></br>de junior a senior</Text>
+            <Text type="h1">Desarrollador de software<br></br>de junior a senior</Text>
             <Text type="h5">Todo el contenido desde un solo click</Text>
             <Btn />
         </div>
