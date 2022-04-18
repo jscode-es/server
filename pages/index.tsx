da555c37ea503c770395aef5d84abbabff7a0c86
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-04-18 14:27:31 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Container from '../src/components/container'
 import ContainerCenter from '../src/components/container-center'
@@ -8,8 +9,13 @@ import CopyRight from '../src/components/copy-right'
 import ContentMain from '../src/components/content-main'
 import Logo from '../src/components/logo'
 import Bg from '../src/components/bg'
+import Nav from '../src/components/nav'
+import Menu from '../src/components/menu'
 
 const Home: NextPage = () => {
+
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
     <>
       <Head>
@@ -19,9 +25,11 @@ const Home: NextPage = () => {
       </Head>
 
       <Main>
+        {/* <Menu showMenu={showMenu} setShowMenu={setShowMenu} /> */}
         <Container>
           <Bg />
           <ContainerCenter>
+            {/* <Nav showMenu={showMenu} setShowMenu={setShowMenu} /> */}
             <Logo />
             <ContentMain />
             <CopyRight />
