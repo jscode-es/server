d693ae46346655fef289a3bbc823329f391bfe93
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-04-19 20:56:02 +0200

    Update index.tsx

--- src/components/bg/index.tsx
+++ src/components/bg/index.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import style from './style.module.css'
 
-//import Image from 'next/image'
+import Image from 'next/image'
+import bg from '../../../public/img/bg.gif'
 
 export default function Bg() {
     return (
         <>
             <div className={style.container}></div>
-            <img className={style.img} src="/img/bg.gif" alt="JSCode" />
+            <Image className={style.img} src={bg} alt="JSCode" layout="fill" />
         </>)
 }
