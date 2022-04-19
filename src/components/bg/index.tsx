c283ebbb1f5bf651dc9245f603d563f93b3ee40b
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-04-20 00:29:23 +0200

    Update index.tsx

--- src/components/bg/index.tsx
+++ src/components/bg/index.tsx
@@ -8,6 +8,7 @@ export default function Bg() {
     return (
         <>
             <div className={style.container}></div>
+            <img src="https://jscode.es/img/bg.gif" alt="JSCode" className={style.img} />
             {/* <Image className={style.img} src={bg} alt="JSCode" layout="fill" /> */}
         </>)
 }
